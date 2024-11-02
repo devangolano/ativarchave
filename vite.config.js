129677a7ee4116d39b46d7998e232e9a1088f13c
alberto <albertoronny237@gmail.com>
2024-11-02 19:08:15 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ativarchave/', // substitua "nome-do-repositorio" pelo nome do seu repositório
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios'],
-    },
-  },
+ 
 });
